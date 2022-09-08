72ccc9ff46831a61540384b4afaf41c0a0e978e6
pramodh <durgapramodh10dj95@gmail.com>
2022-09-08 11:47:23 -0500

    bug fix in v2

--- src/main/java/com/protfolio/comments/controller/CommentController.java
+++ src/main/java/com/protfolio/comments/controller/CommentController.java
@@ -13,7 +13,7 @@ public class CommentController {
     @GetMapping("/all/comments")
     public List<Comment> getAllComments(){
         List<Comment> comments = new ArrayList<>();
-        comments.add(Comment.builder().email("du@gmail.com").comment("Hey There").build());
+        comments.add(Comment.builder().email("ku@gmail.com").comment("Hey There").build());
         return comments;
     }
 }
